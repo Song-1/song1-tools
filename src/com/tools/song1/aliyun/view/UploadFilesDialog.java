33cb9538899f01f7b179a86c04fdb5f467699efa
刘柱栋 <liuzhudong57@126.com>
2014-07-28 18:32:57 +0800

    阿里云文件上传UI提交

--- src/com/tools/song1/aliyun/view/UploadFilesDialog.java
+++ src/com/tools/song1/aliyun/view/UploadFilesDialog.java
@@ -10,6 +10,7 @@ import org.eclipse.swt.widgets.ProgressBar;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.wb.swt.SWTResourceManager;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Group;
 
 public class UploadFilesDialog extends Dialog {
 
@@ -17,6 +18,8 @@ public class UploadFilesDialog extends Dialog {
 	protected Shell shell;
 	private Text text;
 	private Text text_1;
+	private Text text_2;
+	private Text text_3;
 
 	/**
 	 * Create the dialog.
@@ -50,58 +53,81 @@ public class UploadFilesDialog extends Dialog {
 	 */
 	private void createContents() {
 		shell = new Shell(getParent(), SWT.SHELL_TRIM);
-		shell.setSize(722, 401);
+		shell.setSize(722, 524);
 		shell.setText("文件上传");
 		
 		Composite composite_1 = new Composite(shell, SWT.NONE);
-		composite_1.setBounds(10, 10, 686, 343);
+		composite_1.setBounds(0, 10, 706, 476);
 		
 		Composite composite = new Composite(composite_1, SWT.NONE);
-		composite.setLocation(0, 130);
-		composite.setSize(686, 34);
+		composite.setLocation(5, 260);
+		composite.setSize(701, 34);
 		
 		ProgressBar progressBar = new ProgressBar(composite, SWT.NONE);
 		progressBar.setLocation(43, 10);
-		progressBar.setSize(633, 17);
+		progressBar.setSize(648, 17);
 		progressBar.setSelection(34);
 		Label lblNewLabel = new Label(composite, SWT.NONE);
 		lblNewLabel.setBounds(0, 10, 50, 17);
 		lblNewLabel.setText("进度：");
 		
 		Composite composite_2 = new Composite(composite_1, SWT.NONE);
-		composite_2.setBounds(0, 167, 686, 172);
+		composite_2.setBounds(0, 295, 706, 172);
 		
 		text = new Text(composite_2, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL | SWT.CANCEL);
-		text.setBounds(0, 0, 686, 172);
+		text.setBounds(5, 0, 696, 172);
 		
 		Composite composite_3 = new Composite(composite_1, SWT.NONE);
-		composite_3.setBounds(0, 0, 686, 124);
-		
-		text_1 = new Text(composite_3, SWT.BORDER);
-		text_1.setBounds(81, 49, 472, 25);
-		
-		Label lblNewLabel_1 = new Label(composite_3, SWT.NONE);
-		lblNewLabel_1.setFont(SWTResourceManager.getFont("思源黑体 CN Bold", 10, SWT.NORMAL));
-		lblNewLabel_1.setBounds(10, 52, 65, 20);
-		lblNewLabel_1.setText("选择文件：");
-		
-		Button btnNewButton = new Button(composite_3, SWT.NONE);
-		btnNewButton.setFont(SWTResourceManager.getFont("思源黑体 CN Bold", 10, SWT.NORMAL));
-		btnNewButton.setBounds(559, 48, 107, 27);
-		btnNewButton.setText("选   择");
+		composite_3.setBounds(0, 0, 706, 254);
 		
 		Button btnNewButton_1 = new Button(composite_3, SWT.NONE);
 		btnNewButton_1.setFont(SWTResourceManager.getFont("思源黑体 CN Bold", 11, SWT.NORMAL));
-		btnNewButton_1.setBounds(258, 87, 188, 37);
+		btnNewButton_1.setBounds(253, 207, 188, 37);
 		btnNewButton_1.setText("开  始  上  传");
 		
 		Label label = new Label(composite_3, SWT.SEPARATOR | SWT.HORIZONTAL);
-		label.setBounds(0, 33, 686, 2);
+		label.setBounds(0, 33, 706, 2);
 		
 		Label lblNewLabel_2 = new Label(composite_3, SWT.NONE);
 		lblNewLabel_2.setFont(SWTResourceManager.getFont("思源黑体 CN Bold", 12, SWT.NORMAL));
 		lblNewLabel_2.setBounds(10, 0, 277, 33);
 		lblNewLabel_2.setText("上传文件到阿里云：");
+		
+		Group group = new Group(composite_3, SWT.NONE);
+		group.setBounds(10, 139, 691, 62);
+		
+		text_1 = new Text(group, SWT.BORDER | SWT.READ_ONLY);
+		text_1.setLocation(77, 20);
+		text_1.setSize(490, 25);
+		
+		Label lblNewLabel_1 = new Label(group, SWT.NONE);
+		lblNewLabel_1.setBounds(10, 23, 65, 20);
+		lblNewLabel_1.setFont(SWTResourceManager.getFont("思源黑体 CN Bold", 10, SWT.NORMAL));
+		lblNewLabel_1.setText("选择文件：");
+		
+		Button btnNewButton = new Button(group, SWT.NONE);
+		btnNewButton.setBounds(574, 19, 107, 27);
+		btnNewButton.setFont(SWTResourceManager.getFont("思源黑体 CN Bold", 10, SWT.NORMAL));
+		btnNewButton.setText("选   择");
+		
+		Group group_1 = new Group(composite_3, SWT.NONE);
+		group_1.setBounds(10, 39, 686, 94);
+		
+		Label lblNewLabel_3 = new Label(group_1, SWT.NONE);
+		lblNewLabel_3.setFont(SWTResourceManager.getFont("思源黑体 CN Bold", 10, SWT.NORMAL));
+		lblNewLabel_3.setBounds(10, 22, 61, 17);
+		lblNewLabel_3.setText("BUCKET:");
+		
+		text_2 = new Text(group_1, SWT.BORDER | SWT.READ_ONLY);
+		text_2.setBounds(77, 20, 490, 23);
+		
+		Label lblNewLabel_4 = new Label(group_1, SWT.NONE);
+		lblNewLabel_4.setFont(SWTResourceManager.getFont("思源黑体 CN Bold", 10, SWT.NORMAL));
+		lblNewLabel_4.setBounds(10, 55, 61, 17);
+		lblNewLabel_4.setText("上传路径：");
+		
+		text_3 = new Text(group_1, SWT.BORDER | SWT.READ_ONLY);
+		text_3.setBounds(77, 52, 490, 23);
 
 	}
 }
