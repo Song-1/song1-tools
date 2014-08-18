89477f7020bbedfd23783583bb7ac219a2cfed33
刘柱栋 <liuzhudong57@126.com>
2014-08-18 11:16:08 +0800

    调整状元听书和樱桃时光上传数据实现代码的包结构提交

---
+++ src/com/tools/song1/runnable/book/BookDataInfo.java
@@ -0,0 +1,128 @@
+/**
+ * 
+ */
+package com.tools.song1.runnable.book;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.tools.song1.util.FileDoUtil;
+
+/**
+ * @author Administrator
+ *
+ */
+public class BookDataInfo {
+	private String id;
+	private String name;
+	private String typeLevelOne;
+	private String typeLevelTwo;
+	private String author;
+	private String player;
+	private String bookInfo;
+	private String desc;
+	private String img;
+	private List<String> books;
+	private String uuid;
+	public BookDataInfo() {
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		if(name != null){
+			String uuidStr = UUID.randomUUID().toString();
+			uuid = uuidStr.replace("-", "").toUpperCase();
+		}
+		this.name = name;
+	}
+
+	public String getTypeLevelOne() {
+		return typeLevelOne;
+	}
+
+	public void setTypeLevelOne(String typeLevelOne) {
+		this.typeLevelOne = typeLevelOne;
+	}
+
+	public String getTypeLevelTwo() {
+		return typeLevelTwo;
+	}
+
+	public void setTypeLevelTwo(String typeLevelTwo) {
+		this.typeLevelTwo = typeLevelTwo;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(String player) {
+		this.player = player;
+	}
+
+	public String getBookInfo() {
+		return bookInfo;
+	}
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public List<String> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<String> books) {
+		this.books = books;
+	}
+
+	public void addBook(String str) {
+		if (str != null) {
+			books.add(str);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "BookDataInfo [id=" + id + ", name=" + name + ", typeLevelOne=" + typeLevelOne + ", typeLevelTwo=" + typeLevelTwo + ", author=" + author + ", player=" + player + ", bookInfo=" + bookInfo + ", desc=" + desc + ", img=" + img + ", books=" + books + ", uuid=" + uuid + "]";
+	}
+
+}
---
+++ src/com/tools/song1/runnable/book/BookUtil.java
@@ -0,0 +1,141 @@
+/**
+ * 
+ */
+package com.tools.song1.runnable.book;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.tools.song1.aliyun.OSSUploadUtil;
+import com.tools.song1.data.api.BaseResultBean;
+import com.tools.song1.data.api.PageDataModel;
+import com.tools.song1.util.FileDoUtil;
+import com.tools.song1.util.HttpClientUtil;
+import com.tools.song1.util.StringUtil;
+
+/**
+ * @author Administrator
+ *
+ */
+public class BookUtil {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 * @throws ClientProtocolException
+	 */
+	private static String ALIYUNURLBASE = "oss-cn-hangzhou.aliyuncs.com/";
+
+	public static void main(String[] args) throws Exception {
+		String baseUrl = "http://localhost:8080/song1/api/book/v1/list";
+		String bucket = "testupload2";
+		start(bucket, baseUrl);
+		
+	}
+
+	public static void start(String bucket, String baseUrl) {
+		if(StringUtil.isEmptyString(bucket)){
+			System.out.println("阿里云BUCKET不能为空!");
+		}else if(StringUtil.isEmptyString(baseUrl)){
+			System.out.println("服务器获取书籍列表URL不能为空!");
+		}
+		try {
+			int j = baseUrl.lastIndexOf("?");
+			if(j > 0){
+				baseUrl = new String(baseUrl.substring(0,j));
+			}
+			int strat = 0;
+			int pageSize = 100;
+			int counts = 0;
+			do {
+				String url = baseUrl + "?start=" + strat + "&limit=" + pageSize;
+				String content = HttpClientUtil.doGet(url);
+				PageDataModel<BookResponseDataModel> pageData = doData(content);
+				if (pageData != null) {
+					strat += pageSize;
+					counts = pageData.getRecordCount();
+					List<BookResponseDataModel> books = pageData.getListPageObject();
+					if (books != null) {
+						for (BookResponseDataModel bookResponseDataModel : books) {
+							if (bookResponseDataModel != null) {
+								String key = bookResponseDataModel.getUrl();
+								key = StringUtil.decodeURL(key);
+								int i = key.indexOf(ALIYUNURLBASE);
+								if (i > 0) {
+									key = new String(key.substring(i + ALIYUNURLBASE.length()));
+								}
+								i = key.lastIndexOf("?");
+								if (i > 0) {
+									key = new String(key.substring(0, i));
+								}
+								boolean flag = OSSUploadUtil.isObjectExist(bucket, key);
+								if (!flag) {
+									FileDoUtil.outLog(key + " [ 阿里云不存在此文件 ]");
+								}
+								System.out.println(key + " [ 阿里云存在此文件 ]");
+							}
+						}
+					}
+				}
+			} while (counts > strat);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static boolean updateBookDataURLStuffixForDB(String baseURL){
+		if(StringUtil.isEmptyString(baseURL)){
+			System.out.println("服务器更新数据书籍URL的API地址不能为空!");
+			return false;
+		}
+		String stuffix = ".mp3";
+		try{
+			String url = baseURL + "?stuffix=" + stuffix;
+			String content = HttpClientUtil.doGet(url);
+			Gson gson = new Gson();
+			Type type = new TypeToken<BaseResultBean<String>>(){}.getType();
+			BaseResultBean<String> bean = gson.fromJson(content,type);
+			if (bean != null) {
+				if("1000".equals(bean.getStatus())){
+					if ("success".equals(bean.getData())) {
+						return true;
+					}else{
+						System.out.println(bean.getData());
+						return false;
+					}
+				}else{
+					System.out.println(bean.getMessage());
+					return false;
+				}
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return false;
+		
+		
+		
+	}
+
+	public static PageDataModel<BookResponseDataModel> doData(String content) {
+		if (!StringUtil.isEmptyString(content)) {
+			Gson gson = new Gson();
+			Type type = new TypeToken<BaseResultBean<PageDataModel<BookResponseDataModel>>>(){}.getType();
+			BaseResultBean<PageDataModel<BookResponseDataModel>> bean  = gson.fromJson(content,type);
+			if (bean != null) {
+				if("1000".equals(bean.getStatus())){
+					PageDataModel<BookResponseDataModel> pageDate = bean.getData();
+					if (pageDate != null) {
+						return pageDate;
+					}
+				}else{
+					System.out.println(bean.getMessage());
+				}
+			}
+		}
+		return null;
+	}
+
+}
